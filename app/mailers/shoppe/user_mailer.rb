8abb989811594c08db782cbe16bcdf436e746a9f
Jack Clackett <jclackett@gmail.com>
2016-07-26 15:35:18 +0200

    updating the user login, and password reset text

--- app/mailers/shoppe/user_mailer.rb
+++ app/mailers/shoppe/user_mailer.rb
@@ -2,7 +2,7 @@ module Shoppe
   class UserMailer < ActionMailer::Base
     def new_password(user)
       @user = user
-      mail from: Shoppe.settings.outbound_email_address, to: user.email_address, subject: 'Your new Shoppe password'
+      mail from: Shoppe.settings.outbound_email_address, to: user.email_address, subject: 'Your new Hooked Gathering password'
     end
   end
 end
